db532d4cce6ce6679649a59b84cfdebef6c1997b
Albertomarquezzz <albertoomarquezzz@gmail.com>
2023-06-07 12:44:10 +0200

    v4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
 
   tablaLoginBoolean = true;
   usuarioLogueado!: Boolean;
+  showMenu: boolean = false;
 
   constructor(
     private userService: LoginService,
@@ -40,6 +41,9 @@ export class AppComponent {
   }
   irInicio() {
     this.router.navigate(['inicio']);
+  }
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
   }
   onClickLogout() {
     this.userService.logout()
